1bb82e4c8b2129bbb820cc920d805b8119ce1529
Russell Gordon <github@russellgordon.ca>
2022-09-27 12:35:57 -0400

    Now provides starter code that can't be edited and works for drawing shapes.

--- PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/main.swift
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/main.swift
@@ -1,9 +1,30 @@
 //#-hidden-code
-//
-//  See LICENSE folder for this template’s licensing information.
-//
-//  Abstract:
-//  The Swift file containing the source code edited by the user of this playground book.
-//
+
+import PlaygroundSupport
+import SpriteKit
+import BookCore
+
+func addShape(drawnBy: Tortoise){
+    let view = GridPaperView()
+    view.add(turtle)
+    PlaygroundPage.current.liveView = view
+}
 //#-end-hidden-code
-let str = "Hello, playground"
+
+// Create a turtle to draw things for you
+var turtle = Tortoise()
+
+//#-editable-code
+
+// Add code below to make the turtle draw
+// Begin with:
+//
+// turtle.
+//
+// ... and see what the turtle can do for you.
+
+//#-end-editable-code
+
+// Add the shape drawn by the turtle to the sketch
+addShape(drawnBy: turtle)
+
